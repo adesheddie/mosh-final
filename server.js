a731b197b9dc10d263b311fb044175110cc00058
adesheddie <adesh.eddie@gmail.com>
2019-07-26 18:53:47 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+require('./startup/logging')();
+require('./startup/routers')(app);
+require('./startup/db')();
+// require('./startup/configs')();
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(3001);
+  }
+
+console.log('Node Running for Movies API');
+module.exports = app;
