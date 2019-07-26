1e0dcb94d68683a11cb46bf3ff461098b92b4d25
adesheddie <adesh.eddie@gmail.com>
2019-07-26 19:01:53 +0530

    First Commit

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ require('./startup/routers')(app);
 require('./startup/db')();
 // require('./startup/configs')();
 
-if (process.env.NODE_ENV !== 'test') {
+
     app.listen(process.env.PORT || 3001);
-  }
+ 
 
 console.log('Node Running for Movies API');
 module.exports = app;
